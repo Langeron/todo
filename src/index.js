51e64d28d8d44de66c6918fb2f865e088be47c24
Dima Goncharenko <dgoncharenko@crispwp.com>
2020-01-23 18:47:27 +0200

    First component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log(55)
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const TodoList = () => {
+	return (
+		<ul>
+			<li>Item 1</li>
+			<li>Item 2</li>
+		</ul>
+	)
+}
+
+const el = (
+	<div>
+		<h1>Hello world</h1>
+		<input type="search"/>
+		<TodoList/>
+	</div>
+);
+
+ReactDOM.render(el, document.getElementById('root'));
