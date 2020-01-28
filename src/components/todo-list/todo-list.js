3c9f75cb2b4b261d92c9f50a543418e069e8bedf
Dima Goncharenko <dgoncharenko@crispwp.com>
2020-01-28 16:23:40 +0200

    Update state important and done

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -3,7 +3,11 @@ import TodoListItem from '../todo-list-item';
 
 import './todo-list.css';
 
-const TodoList = ({ todos, onDeleted }) => {
+const TodoList = ({ 
+	todos, 
+	onDeleted, 
+	onToggleImportant,
+	onToggleDone }) => {
 
 
 	const elements = todos.map((item) => {
@@ -13,7 +17,9 @@ const TodoList = ({ todos, onDeleted }) => {
 		return (
 			<li key={item.id} className="list-group-item">
 				<TodoListItem {...itemProps} 
-				onDeleted={() => onDeleted(id)}/>
+				onDeleted={() => onDeleted(id)}
+				onToggleImportant={() => onToggleImportant(id)}
+				onToggleDone={() => onToggleDone(id)} />
 			</li>
 		)
 	});
